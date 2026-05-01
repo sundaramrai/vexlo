e2079743a4b8cfa12f6165a70e46b33c0672d4e6
Sundaram Rai <sundaramrai02@gmail.com>
2026-05-02 01:04:21 +0530

    refactor handler.go to embed and serve dashboard assets (HTML, CSS, JS)

--- internal/dashboard/handler.go
+++ internal/dashboard/handler.go
@@ -8,7 +8,28 @@ import (
 //go:embed dashboard.html
 var dashboardHTML []byte
 
+//go:embed dashboard.css
+var dashboardCSS []byte
+
+//go:embed dashboard.js
+var dashboardJS []byte
+
 func ServeHTML(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	_, _ = w.Write(dashboardHTML)
+	serveAsset(w, "text/html; charset=utf-8", dashboardHTML)
+}
+
+func ServeCSS(w http.ResponseWriter, _ *http.Request) {
+	serveAsset(w, "text/css; charset=utf-8", dashboardCSS)
+}
+
+func ServeJS(w http.ResponseWriter, _ *http.Request) {
+	serveAsset(w, "text/javascript; charset=utf-8", dashboardJS)
+}
+
+func serveAsset(w http.ResponseWriter, contentType string, body []byte) {
+	w.Header().Set("Content-Type", contentType)
+	w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+	w.Header().Set("Pragma", "no-cache")
+	w.Header().Set("Expires", "0")
+	_, _ = w.Write(body)
 }
