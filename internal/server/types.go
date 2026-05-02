58a1db742b989098bbb4b92fc7dc2b0c6d7c607d
Sundaram Rai <sundaramrai02@gmail.com>
2026-05-02 21:57:02 +0530

    remove unused RoutingRule type alias from types.go

--- internal/server/types.go
+++ internal/server/types.go
@@ -2,7 +2,6 @@ package server
 
 import "vexlo/internal/model"
 
-type RoutingRule = model.RoutingRule
 type Session = model.Session
 type CapturedRequest = model.CapturedRequest
 type CapturedReplay = model.CapturedReplay
