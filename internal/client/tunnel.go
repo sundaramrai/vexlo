14aef3d17d60dcf91456e0ff357ece42ad26043c
Sundaram Rai <sundaramrai02@gmail.com>
2026-05-02 21:59:59 +0530

    refactor: remove unused SSH channel connection code from tunnel.go

--- internal/client/tunnel.go
+++ internal/client/tunnel.go
@@ -3,31 +3,8 @@ package client
 import (
 	"bufio"
 	"net"
-	"time"
-
-	"golang.org/x/crypto/ssh"
 )
 
 func newReader(conn net.Conn) *bufio.Reader {
 	return bufio.NewReader(conn)
 }
-
-type sshChannelConn struct {
-	ssh.Channel
-	client *ssh.Client
-}
-
-func (c sshChannelConn) LocalAddr() net.Addr                { return dummyAddr("ssh-local") }
-func (c sshChannelConn) RemoteAddr() net.Addr               { return dummyAddr("ssh-remote") }
-func (c sshChannelConn) SetDeadline(t time.Time) error      { return nil }
-func (c sshChannelConn) SetReadDeadline(t time.Time) error  { return nil }
-func (c sshChannelConn) SetWriteDeadline(t time.Time) error { return nil }
-func (c sshChannelConn) Close() error {
-	_ = c.Channel.Close()
-	return c.client.Close()
-}
-
-type dummyAddr string
-
-func (d dummyAddr) Network() string { return string(d) }
-func (d dummyAddr) String() string  { return string(d) }
